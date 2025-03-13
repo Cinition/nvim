9a510fed56d45d155931b27c45725676bef55208
Cornee Nieuwenhuis <37161255+Cinition@users.noreply.github.com>
2025-03-13 08:55:46 +0100

    Fix lsp keymaps

--- after/config/plugins/telescope.lua
+++
@@ -1,7 +0,0 @@
-vim.keymap.set('n', 'gr', require('telescope.builtin').lsp_references) -- Only show filename
-vim.keymap.set('n', 'gd', require('telescope.builtin').lsp_definitions) -- Only show filename
-vim.keymap.set('n', 'gi', require('telescope.builtin').lsp_implementations) -- Only show filename
-vim.keymap.set('n', '<leader>td', require('telescope.builtin').lsp_type_definitions)
-vim.keymap.set('n', '<leader>ds', require('telescope.builtin').lsp_document_symbols)
-vim.keymap.set('n', '<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols)
-
--- lua/config/plugins/lsp.lua
+++ lua/config/plugins/lsp.lua
@@ -19,8 +19,26 @@ return {
             require('lspconfig').zls.setup { capabilities = capabilities }
             require('lspconfig').clangd.setup { capabilities = capabilities }
 
+
             vim.keymap.set("n", "<leader>f", function() vim.lsp.buf.format() end)
             vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end)
+
+            vim.api.nvim_create_autocmd('LspAttach', {
+                group = vim.api.nvim_create_augroup('lsp-attach', { clear = true }),
+                callback = function(event)
+                    local map = function(keys, func)
+                        vim.keymap.set('n', keys, func, { buffer = event.buf })
+                    end
+
+                    map('gr', require('telescope.builtin').lsp_references)
+                    map('gd', require('telescope.builtin').lsp_definitions)
+                    map('gi', require('telescope.builtin').lsp_implementations)
+
+                    map('<leader>td', require('telescope.builtin').lsp_type_definitions)
+                    map('<leader>ds', require('telescope.builtin').lsp_document_symbols)
+                    map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols)
+                end
+            });
         end,
     }
 }
